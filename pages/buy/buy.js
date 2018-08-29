f656dc21359c83183815832005ed678aa2c43d70
huhao666 <782044678@qq.com>
2018-08-29 16:05:51 +0800

    8-29

--- pages/buy/buy.js
+++ pages/buy/buy.js
@@ -4,16 +4,32 @@ const app = getApp()
 
 Page({
   data: {
-    num:1
+    num:1, //数量
+    dates:[
+      { "sku": "01F02E074", "name": "十三" },
+      { "sku": "01F02E075", "name": "十四" },
+      { "sku": "01F02E076", "name": "十五" },
+      { "sku": "01F02E077", "name": "十六" },
+      { "sku": "01F02E078", "name": "十七" },
+      { "sku": "01F02E079", "name": "十八" },
+      { "sku": "01F02E070", "name": "十九" },
+    ],
+    state: ''//选择sku
   },
   onLoad: function () {
     
   },
   onChange(e){   
     this.setData({ num: e.detail });
-    
   },
   addcart(){
     console.log(this.data.num)
+  },
+  //选择sku加样式
+  select_sku: function (e) {
+    console.log(e.currentTarget.dataset.key)
+    this.setData({
+      state: e.currentTarget.dataset.key,
+    });
   }
 })
--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -12,8 +12,6 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
     indicatorDots: true,
-    autoplay: false,
-    interval: 5000,
     duration: 1000,
     title: "EPT20 - 15ET2 1.5t全电动搬运车 中力小金刚二代",
     amPrice1:9900,      //阿母价格
@@ -22,13 +20,14 @@ Page({
     DLPrice2: 10000,
     saleNumber:122,     //已售数量
     show: false,        //弹出层
-    content:"内容"
+    content:"内容",
+    shopId:123
   },
   onLoad: function (options) {
     //console.log(options);
     this.init();
   },
-  //事件处理函数
+  //分享
   onShareAppMessage: function () {
     console.log(this.data.title)
     return {
@@ -36,22 +35,32 @@ Page({
       path: '/pages/productDetail/productDetail'
     }
   },
-  showA:function(){
-    this.setData({ show: true });
-  },
   //弹框
+  showPopup(e) {
+    // let showNo = e.currentTarget.dataset.show;
+    // if (showNo == 1) {
+    //   let showContent = this.data.businessLicense;
+    //   this.setData({ showContent: showContent });
+    // } else if (showNo == 2) {
+    //   let showContent = this.data.card;
+    //   this.setData({ showContent: showContent });
+    // } else {
+    //   let showContent = this.data.wechat;
+    //   this.setData({ showContent: showContent });
+    // };
+    this.setData({
+      show: !this.data.show
+    });
+  },
   onClose() {
     this.setData({ show: false });
   },  
-  //下拉刷新
-  onPullDownRefresh() {
-    this.init();
-    wx.stopPullDownRefresh();
-  },
   //页面初始化
   init() {
-    //获取首页轮播图的内容
-    getProductDetail("POST").then(res => {
+    //获取产品详情内容
+    getProductDetail("POST",{
+      id: this.data.shopId
+    }).then(res => {
       console.log(res)
       // this.setData({
       //   "bannerList": res.data.bannerItemList
