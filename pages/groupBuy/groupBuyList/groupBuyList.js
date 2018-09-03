7d34b171d107012b69b48b74f324e4c669752e6b
wangjinxu <1576520768@qq.com>
2018-09-03 20:39:42 +0800

    feat(page): add groupBuy pages

---
+++ pages/groupBuy/groupBuyList/groupBuyList.js
@@ -0,0 +1,31 @@
+
+
+Page({
+  data: {
+    list: [
+      {
+        id: "101",
+        title: "我发起的拼团"
+      },
+      {
+        id: "102",
+        title: "我参与的拼团"
+      }
+    ],
+    selectedId: "101",
+    statusList: [
+      {
+        id: "1101",
+        title: "全部"
+      },
+      {
+        id: "1102",
+        title: "进行中"
+      },
+      {
+        id: "1103",
+        title: "已结束"
+      }
+    ]
+  }
+})
