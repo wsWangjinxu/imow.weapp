8c60b29fae9f51fa7c36304c6da2b525ccd88bb3
wangjinxu <1576520768@qq.com>
2018-10-16 10:57:46 +0800

    feat(fixed): fixed login

--- pages/my/my.js
+++ pages/my/my.js
@@ -3,8 +3,6 @@ import {
   getThirdSession
 } from "../../api/user/manage"
 
-let app = getApp();
-
 Page({
   data: {
     array: ['全部明细', '支付明细'],
@@ -12,68 +10,60 @@ Page({
     showLogin: true,
     nickname: "",
     avatarUrl: "",
-    isLogin: false,
+    isLogin: true,
     account: "",
     password: ""
   },
 
-    //获取用户的微信信息，头像和昵称
-    getUserWXInfo() {
-      let that = this;
-      wx.getUserInfo({
-        success: function(res) {
-          let tempData = res.rawData;
-          //数据转换
-          tempData = JSON.parse(tempData);
-          that.setData({
-            nickname: tempData.nickName,
-            avatarUrl: tempData.avatarUrl
-          })
-        },
-        fail: function(err) {
-          wx.showToast({
-            title: "获取信息出错",
-            image: "/static/icon/warning-white.png"
-          })
-        }
-      })
-    },
+
 
   onLoad() {
     //flag用来标识Page页面的onLoad是否先于app.js中异步的执行
     //这种情况用于用户已经在服务器上边绑定了阿母账号，如果没有绑定阿母账号，则获取缓存isLogin为false
-    if (app.globalData.flag === "-1") {
-      //此条件用于page的onLoad方法先于app请求数据的回调执行
-      let that = this;
-      app.callback = function () {
-        let isLogin = wx.getStorageSync("isLogin");
-        //判断用户已经在平台上绑定微信账号，则记录登陆状态，显示我的页面，并获取头像和昵称
-        if (isLogin) {
-          //登陆成功以后获取用户的微信昵称和头像
-          this.getUserWXInfo();
+    // if (app.globalData.flag === "-1") {
+    //   //此条件用于page的onLoad方法先于app请求数据的回调执行
+    //   let that = this;
+    //   app.callback = function () {
+    //     let isLogin = wx.getStorageSync("isLogin");
+    //     //判断用户已经在平台上绑定微信账号，则记录登陆状态，显示我的页面，并获取头像和昵称
+    //     if (isLogin) {
+    //       //登陆成功以后获取用户的微信昵称和头像
+    //       this.getUserWXInfo();
 
-          //设置登陆状态
-          that.setData({
-            isLogin: isLogin
-          });
-        }
-      }
-    } else {
-      let isLogin = wx.getStorageSync("isLogin");
-      //判断用户已经在平台上绑定微信账号，则记录登陆状态，显示我的页面，并获取头像和昵称
-      if (isLogin) {
-        //登陆成功以后获取用户的微信昵称和头像
-        this.getUserWXInfo();
-
+    //       //设置登陆状态
+    //       that.setData({
+    //         isLogin: isLogin
+    //       });
+    //     }
+    //   }
+    // } else {
+    let isLogin = wx.getStorageSync("isLogin");
+    //判断用户已经在平台上绑定微信账号，则记录登陆状态，显示我的页面，并获取头像和昵称
+    if (isLogin) {
+      let _this = this;
+      wx.showLoading({
+        title: '玩命加载中'
+      });
+      //登陆成功以后获取用户的微信昵称和头像
+      this.getUserWXInfo(function () {
         //设置登陆状态
-        this.setData({
-          isLogin: isLogin
-        });
-      }
+        wx.hideLoading();
+      });
+    } else {
+      this.setData({
+        isLogin: false
+      }, function() {
+        wx.hideLoading();
+      })
     }
+    // }
   },
 
   login() {
+    wx.showLoading({
+      title: '玩命加载中'
+      // mask: 'true'
+    });
     let account = this.data.account;
     let password = this.data.password;
     let _this = this;
@@ -92,20 +82,18 @@ Page({
                 //登陆成功，设置缓存
                 wx.setStorageSync("session", res.data.userInfo.token);
                 wx.setStorageSync("isLogin", true);
-                
+
                 //获取用户的头像和昵称
-                wx.getUserInfo({
-                  success: function(res) {
-                    console.log(res);
-                  },
-                  fail: function(res) {
-                    console.log(err);
+                _this.getUserWXInfo(
+                  function () {
+                    wx.hideLoading();
+                    _this.setData({
+                      isLogin: true
+                    });
                   }
-                })
-                _this.setData({
-                  isLogin: true
-                });
+                );
               } else {
+                wx.hideLoading();
                 wx.showToast({
                   title: res.data.message,
                   image: "/static/icon/warning-white.png"
@@ -113,10 +101,16 @@ Page({
               }
             });
           } else {
+            wx.hideLoading();
+            wx.showToast({
+              title: '获取code失败',
+              image: '/static/icon/warning-white.png'
+            })
             console.log("登陆失败！" + res.errMsg);
           }
         },
         fail() {
+          wx.hideLoading();
           console.log("登陆时网络错误");
         }
       });
@@ -142,6 +136,29 @@ Page({
     this.setData({
       password: e.detail.value
     });
-  }
+  },
+
+  //获取用户的微信信息，头像和昵称
+  getUserWXInfo(complete) {
+    let that = this;
+    wx.getUserInfo({
+      success: function (res) {
+        let tempData = res.rawData;
+        //数据转换
+        tempData = JSON.parse(tempData);
+        that.setData({
+          nickname: tempData.nickName,
+          avatarUrl: tempData.avatarUrl
+        })
+      },
+      fail: function (err) {
+        wx.showToast({
+          title: "获取信息出错",
+          image: "/static/icon/warning-white.png"
+        })
+      },
+      complete
+    })
+  },
 
 })
